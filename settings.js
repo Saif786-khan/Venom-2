088ca543f2a306c612df131a5c07e73d90ae0a9f
Saif786-khan <139172758+Saif786-khan@users.noreply.github.com>
2024-07-25 14:10:05 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,7 +4,7 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923242674043"
+global.ownernomer = "923235587941"
 global.ownername = "LEGEND"
 global.ytname = "LEGEND"
 global.socialm = "GitHub: DeeCeeXxx"
